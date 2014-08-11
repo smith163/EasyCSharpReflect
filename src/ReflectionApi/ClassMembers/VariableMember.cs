fb4c3e08d191b2aae3b7a1cf9cbc17ef882515cc
Walzon <wilsonkaodev@gmail.com>
2014-08-10 21:18:15 -0400

    Add Interface project

--- src/ReflectionApi/ClassMembers/VariableMember.cs
+++ src/ReflectionApi/ClassMembers/VariableMember.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReflectionApi.Interface;
 
 namespace ReflectionApi.ClassMembers
 {
-    public abstract class VariableMember
+    public abstract class VariableMember : IVariableMember
     {
-        public string Name;
-        public Type Type;
-        public object Value;
+        public string Name { get; set; }
+        public Type Type { get; set; }
+        public object Value { get; set; }
 
         protected VariableMember(string Name, Type type)
         {
